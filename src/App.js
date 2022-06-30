9c1d9ced16908e59443e1eee779eea8f6cf8f1c1
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-30 08:52:21 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
 import Hangman from "./components/Hangman";
+import Playing from "./components/Playing";
 
 const App = () =>{
+
+    const [head, setHead] = React.useState(false);
+    const [body, setBody] = React.useState(false);
+    const [armOne, setArmOne] = React.useState(false);
+    const [armTwo, setArmTwo] = React.useState(false);
+    const [legOne, setLegOne] = React.useState(false);
+    const [legTwo, setLegTwo] = React.useState(false);
+
+    const [playing, setPlaying] = React.useState(false);
+
+    const playAgain = () =>{
+        setPlaying(true);
+    };
+
     return(
 
         <div>
             
-            <Hangman />
+            <Hangman 
+                head={head}
+                body={body}
+                armOne={armOne}
+                armTwo={armTwo}
+                legOne={legOne}
+                legTwo={legTwo}
+            />
+
+            <Playing 
+                playing={playing}
+                playAgain={playAgain}
+            />
 
         </div>
 
