7a2898238775060b9fa8c2523a8fd6ee16b472dd
Milos Milijasevic <milosrm20005@gmail.com>
2022-06-30 08:52:42 +0200

    hangman-js updated

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,25 +1,17 @@
 import React from "react";
 import "./Hangman.css";
 
-const Hangman = () =>{
-
-    const [head, setHead] = React.useState(false);
-    const [body, setBody] = React.useState(false);
-    const [armOne, setArmOne] = React.useState(false);
-    const [armTwo, setArmTwo] = React.useState(false);
-    const [legOne, setLegOne] = React.useState(false);
-    const [legTwo, setLegTwo] = React.useState(false);
-
+const Hangman = (props) =>{
     return(
 
         <div className="overlay-hangman">
 
-            <div className={head === true ? "head" : "transparent-head"}></div>
-            <div className={body === true ? "body" : "transparent-body"}></div>
-            <div className={armOne === true ? "arm-one" : "transparent-arm-one"}></div>
-            <div className={armTwo === true ? "arm-two" : "transparent-arm-two"}></div>
-            <div className={legOne === true ? "leg-one" : "transparent-leg-two"}></div>
-            <div className={legTwo === true ? "leg-two" : "transparent-leg-two"}></div>
+            <div className={props.head === true ? "head" : "transparent-head"}></div>
+            <div className={props.body === true ? "body" : "transparent-body"}></div>
+            <div className={props.armOne === true ? "arm-one" : "transparent-arm-one"}></div>
+            <div className={props.armTwo === true ? "arm-two" : "transparent-arm-two"}></div>
+            <div className={props.legOne === true ? "leg-one" : "transparent-leg-two"}></div>
+            <div className={props.legTwo === true ? "leg-two" : "transparent-leg-two"}></div>
             <div className="layer-one"></div>
             <div className="layer-two"></div>
             <div className="layer-three"></div>
