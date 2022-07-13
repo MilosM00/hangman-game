88c71fcaf00947ed48e15967297e3baf07a7a1f9
Milos Milijasevic <milosrm20005@gmail.com>
2022-07-13 10:26:33 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Hangman from "./components/Hangman";
 
 const App = () =>{
     return(
 
         <div>
-            
+
+            <Hangman />
+
         </div>
 
     );
